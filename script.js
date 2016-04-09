46912703c8e85df39444fb77e6e3fcea873adb0f
ParkPants <steven.j.park.mail@gmail.com>
2016-04-09 11:26:48 -0400

    Added Libaries and initial script

---
+++ script.js
@@ -0,0 +1,14 @@
+//Play musical note based on inputted value
+function playNote(inputValue) {
+  MIDI.loadPlugin({
+    instrument: "acoustic_grand_piano",
+    onsuccess: function(){
+      var delay = 0;
+      var note = inputValue; //Currently only accepts integer values
+      var velocity = 120; //How hard the note is played
+      MIDI.setVolume(0,120);
+      MIDI.noteOn(0, note, velocity, delay);
+      MIDI.noteOff(0, note, delay + 0.50);
+    }
+  });
+}
